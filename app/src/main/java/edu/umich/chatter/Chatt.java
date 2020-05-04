ad41d66a6ba9435055f68303e4ebe0a4e4dc7d70
Jerry Wang <jerwang@umich.edu>
2020-05-04 16:18:53 -0400

    Done with front-end

---
+++ app/src/main/java/edu/umich/chatter/Chatt.java
@@ -0,0 +1,12 @@
+package edu.umich.chatter;
+
+public class Chatt {
+    public String username;
+    public String message;
+    public String timestamp;
+    public Chatt(String username, String message, String timestamp) {
+        this.username = username;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
